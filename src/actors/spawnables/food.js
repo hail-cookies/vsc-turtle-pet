886068899334cea889e4f6ebf911605951171bba
sHustinx <91604850+sHustinx@users.noreply.github.com>
2026-05-25 14:36:33 +0200

    new: added shadow and animation to food

--- src/actors/spawnables/food.js
+++ src/actors/spawnables/food.js
@@ -6,9 +6,22 @@ export class Food extends Spawnable {
 
     onSpawn(data) {
         this.element.textContent = this.icons[Math.floor(Math.random() * this.icons.length)];
+        
+        // add food and enter classes before showing so we get a drop-in animation
+        this.element.classList.add('food', 'enter');
         super.onSpawn(data);
+
+        // match the positioning of the turtle: anchor to the bottom with a percentage
         this.posY = this.floorHeight;
-        this.element.style.top = this.posY + 'px';
+        this.element.style.top = 'auto';
+        this.element.style.bottom = '12%';
+
+        // add shadow
+        this.element.style.setProperty('--shadow-size', (this.element.offsetWidth * 1.2) + 'px');
+
+        // animate in
+        setTimeout(() => this.element.classList.remove('enter'), 350);
+
         Events.trigger('dinnerIsServed', { food: this });
     }
 }
